84e47f62ff7a8fd90cc9c31fa01ec2a536b9dc6a
yuri_sn <yuri.snitko@gmail.com>
2024-05-05 00:21:20 +0300

    add changes

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/yuri_sn/Projects/flowers-delivery/flowers-delivery-app/app/layout\":[\"static/media/c9a5bc6a7c948fb0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
