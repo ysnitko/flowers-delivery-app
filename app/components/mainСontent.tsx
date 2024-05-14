682e57fd3a6ad87b279075390e37aee4b0401fc9
yuri_sn <yuri.snitko@gmail.com>
2024-05-14 22:59:52 +0300

    add changes

--- app/components/mainСontent.tsx
+++ app/components/mainСontent.tsx
@@ -3,25 +3,32 @@ import Image from 'next/image';
 export default function MainСontent() {
   return (
     <div className="grid grid-cols-2">
-      <div className="flex flex-col">
-        <div>
-          <p className="text-[67px] after:content-['&reg;']">
+      <div className="flex flex-col p-[80px]">
+        <div className="flex flex-col gap-4 border-b-[1px] border-border-table">
+          <p className="text-[67px] leading-[70px]">
             Kyiv
-            <br /> LuxeBouquets
+            <br /> LuxeBouquet
+            <span className="relative">
+              s
+              <span className="text-3xl absolute left-4 top-1 font-bold">
+                &reg;
+              </span>
+            </span>
           </p>
-          <p>
+          <p className="mb-[54px]">
             Discover Uniquely Crafted Bouquets and Gifts for Any Occasion:
-            Spread Joy with Our Online Flower Delivery Service
+            Spread Joy with Our
+            <span className="italic">Online Flower Delivery Service</span>
           </p>
         </div>
-        <div>
+        <div className="grid grid-cols-2 pt-6 ">
           <Image
             src="/assets/imghero.png"
             alt="img-hero"
             width={256}
             height={256}
           />
-          <p>
+          <p className="flex items-end text-base pl-6 border-l-[1px] border-border-table">
             Experience the joy of giving with our modern floral studio. Order
             online and send fresh flowers, plants and gifts today.
           </p>
