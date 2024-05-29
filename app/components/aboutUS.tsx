589d5632eab500b56d52c0848dd103c0ba11d44d
yuri_sn <yuri.snitko@gmail.com>
2024-05-29 22:54:09 +0300

    add about us

---
+++ app/components/aboutUS.tsx
@@ -0,0 +1,23 @@
+export default function aboutUS() {
+  return (
+    <section>
+      <h1>About us</h1>
+      <div>
+        <p>OUR STORY</p>
+        <p>Kyiv LuxeBouquets</p>
+        <p>
+          We are a modern local floral studio, which specializes in the design
+          and delivery of unique bouquets. We have the best florists who
+          carefully select each look, our studio cooperates directly with farms
+          for growing different flowers, so we always have fresh flowers, which
+          are collected by our florists in exquisite bouquets. We have a
+          collection of fresh bouquets, collections of dried bouquets, house
+          plants, as well as fragrant candles from luxury brands to create the
+          perfect atmosphere. Make someone's day amazing by sending flowers,
+          plants and gifts the same or next day. Ordering flowers online has
+          never been easier.
+        </p>
+      </div>
+    </section>
+  );
+}
