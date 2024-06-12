d3cc783741b1c4e00309065287449bc0fb1479de
yuri_sn <yuri.snitko@gmail.com>
2024-06-12 22:51:27 +0300

    add contact

--- app/components/contactUs.tsx
+++ app/components/contactUs.tsx
@@ -1,4 +1,6 @@
 import Image from 'next/image';
+import Link from 'next/link';
+import { CONTACTS } from '../lib/contacts';
 
 export default function ContactUs() {
   return (
@@ -20,30 +22,56 @@ export default function ContactUs() {
             </div>
           </div>
         </div>
-        <div className="flex  ">
-          <div>
+        <div className="grid grid-cols-2 border-[1px] border-border-table border-r-0 border-b-0 h-full">
+          <div className="border-r-[1px] border-border-table">
             <p className="px-[10px] py-4 text-[38px]">Phone</p>
-            <div className="flex flex-col">
-              <input
-                className="text-base"
-                type="tel"
-                name=""
-                id=""
-                value="+380980099777"
-              />
-              <input
-                className="text-base"
-                type="tel"
-                name=""
-                id=""
-                value="+380980099111"
-              />
+            <div className="flex flex-col p-[50px] gap-5 justify-center items-center">
+              <label htmlFor="tel" className="flex gap-2">
+                <Image
+                  src="/assets/call.svg"
+                  alt="phone"
+                  width={24}
+                  height={24}
+                />
+                <input
+                  className="text-base"
+                  type="tel"
+                  name="tel"
+                  id="tel"
+                  value="+380980099777"
+                />
+              </label>
+              <label htmlFor="tel_tw" className="flex gap-2">
+                <Image
+                  src="/assets/call.svg"
+                  alt="phone"
+                  width={24}
+                  height={24}
+                />
+                <input
+                  className="text-base"
+                  type="tel"
+                  name="tel_tw"
+                  id="tel_tw"
+                  value="+380980099111"
+                />
+              </label>
             </div>
           </div>
           <div>
             <p className="px-[10px] py-4 text-[38px]">Address</p>
-            <p className="text-sm">OPENING HOURS: 8 TO 11 P.M.</p>
-            <p className="text-base">15/4 Khreshchatyk Street, Kyiv </p>
+            <div className="p-[50px] flex flex-col gap-4 items-center">
+              <p className="text-sm">OPENING HOURS: 8 TO 11 P.M.</p>
+              <div className="flex gap-2">
+                <Image
+                  src="/assets/pin_drop.svg"
+                  alt="phone"
+                  width={24}
+                  height={24}
+                />
+                <p className="text-base">15/4 Khreshchatyk Street, Kyiv </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -56,6 +84,27 @@ export default function ContactUs() {
           width={720}
           height={720}
         />
+        <div className="flex flex-row  items-center  border-r-[1px] border-border-table">
+          <p className="text-[38px] px-10 py-4 border-r-[1px] border-l-[1px] border-border-table">
+            Follow us
+          </p>
+          <ul className="flex gap-8 px-8 py-4">
+            {CONTACTS.map((brand) => {
+              return (
+                <li>
+                  <Link href="/">
+                    <Image
+                      src={brand.src}
+                      alt={brand.name}
+                      width={24}
+                      height={24}
+                    />
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
     </section>
   );
---
+++ app/lib/contacts.ts
@@ -0,0 +1,22 @@
+export const CONTACTS = [
+  {
+    name: 'Brands_Instagram',
+    src: '/assets/Brands_Instagram.svg',
+  },
+  {
+    name: 'Brands_Pinterest',
+    src: '/assets/Brands_Pinterest.svg',
+  },
+  {
+    name: 'Brands_Facebook',
+    src: '/assets/Brands_Facebook.svg',
+  },
+  {
+    name: 'Brands_Twitter',
+    src: '/assets/Brands_Twitter.svg',
+  },
+  {
+    name: 'Brands_Telegram',
+    src: '/assets/Brands_Telegram.svg',
+  },
+];
