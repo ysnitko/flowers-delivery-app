5aa9e41813d92e6df29577b944e119b7a158e796
yuri_sn <yuri.snitko@gmail.com>
2024-07-13 01:19:53 +0300

    add changes

--- app/components/carouselProducts.tsx
+++ app/components/carouselProducts.tsx
@@ -1,6 +1,7 @@
-import { Carousel } from 'react-responsive-carousel';
+import Slider from 'react-slick';
 import Image from 'next/image';
-
+import './carousel.css';
+import { url } from 'inspector';
 interface ProductCombinationProps {
   productSelection:
     | {
@@ -26,20 +27,87 @@ interface ProductCombinationProps {
     | undefined;
 }
 
+function SampleNextArrow(props: any) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: 'block', background: 'red' }}
+      onClick={onClick}
+    />
+  );
+}
+
+function SamplePrevArrow(props: any) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{
+        ...style,
+        display: 'block',
+        // backgroundImage: `url('/public/assets/prev.png')`,
+        // backgroundRepeat: 'no-repeat',
+        width: '34px',
+        height: '34px',
+      }}
+      onClick={onClick}
+    />
+  );
+}
+
 export default function CarouselProducts({
   productSelection,
 }: ProductCombinationProps) {
   console.log(productSelection?.Combination);
 
+  const settings = {
+    className: 'slick-container flex w-full',
+    dots: false,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    prevArrow: <SamplePrevArrow />,
+    nextArrow: <SampleNextArrow />,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
   return (
-    <Carousel className="flex flex-row" showArrows={true}>
-      {productSelection?.Combination.map((item) => (
-        <div key={item.id} className=" w-100">
-          <Image src={item.src} width={100} height={100} alt="" />
-          <p className="text-sm ">{item.title}</p>
-          <p className="text-sm text-text-footer">${item.price}</p>
-        </div>
-      ))}
-    </Carousel>
+    <div className="slider-container">
+      <Slider {...settings} className="flex flex-row">
+        {productSelection?.Combination.map((item) => (
+          <div key={item.id} className="flex flex-col gap-10">
+            <Image src={item.src} width={100} height={100} alt="" />
+            <p className="text-sm ">{item.title}</p>
+            <p className="text-sm text-text-footer">${item.price}</p>
+          </div>
+        ))}
+      </Slider>
+    </div>
   );
 }
